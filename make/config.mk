f0458715dfda99192306a7f1cc6fddb041817329
段瑾 <duanjin@meituan.com>
2016-08-31 15:46:49 +0800

    merge config

--- make/config.mk
+++ make/config.mk
@@ -3,11 +3,11 @@
 #
 #  If you want to change the configuration, please use the following
 #  steps. Assume you are on the root directory of xgboost.
-#  First copy this file so that any local changes will be ignored by git
+#  First copy the this file so that any local changes will be ignored by git
 #
 #  $ cp make/config.mk .
 #
-#  Next modify the according entries in the copied file and then compile by
+#  Next modify the according entries, and then compile by
 #
 #  $ make
 #
@@ -24,14 +24,13 @@
 # the additional link flags you want to add
 ADD_LDFLAGS =
 
-# the additional compile flags you want to add
-ADD_CFLAGS =
+ADD_CFLAGS = -Wl,-rpath=/opt/meituan/gcc-4.8.2/lib64 -I/opt/meituan/wormhole/deps/include -Wl,-rpath=/opt/meituan/wormhole/deps/lib
 
 # Whether enable openmp support, needed for multi-threading.
 USE_OPENMP = 1
 
 # whether use HDFS support during compile
-USE_HDFS = 0
+USE_HDFS = 1
 
 # whether use AWS S3 support during compile
 USE_S3 = 0
@@ -39,14 +38,20 @@ USE_S3 = 0
 # whether use Azure blob support during compile
 USE_AZURE = 0
 
+RABIT=/opt/meituan/kongdongying/dmlc/rabit
+
+DMLC_CORE=/opt/meituan/kongdongying/dmlc/dmlc-core
+
+CXX=/opt/meituan/gcc-4.8.2/bin/g++
+CC=/opt/meituan/gcc-4.8.2/bin/gcc
+
 # Rabit library version,
 # - librabit.a Normal distributed version.
 # - librabit_empty.a Non distributed mock version,
 LIB_RABIT = librabit.a
 
-# path to libjvm.so
-LIBJVM=$(JAVA_HOME)/jre/lib/amd64/server
-
+# path to libjvm.soLIBJVM=$(JAVA_HOME)/jre/lib/amd64/server
+LIBJVM=/usr/local/java/jre/lib/amd64/server
 # List of additional plugins, checkout plugin folder.
 # uncomment the following lines to include these plugins
 # you can also add your own plugin like this
